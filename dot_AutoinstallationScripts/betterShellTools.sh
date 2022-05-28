99ec8a9a1316cdcc14f41bb2f9abb46befcdc23b
andy <445998251@qq.com>
2022-05-29 00:17:09 +0800

    Add pwndbg in shell installation script

--- dot_AutoinstallationScripts/betterShellTools.sh
+++ dot_AutoinstallationScripts/betterShellTools.sh
@@ -8,4 +8,9 @@ sudo apt-get install htop
 # Ripgrep is a recursive line-oriented CLI search tool. Aims to be a faster alternative to grep
 sudo apt-get install rg
 
+# pwndbg to improve gdb 
+# this will add to current directory but can modified in ~/.gdbinit
+git clone https://github.com/pwndbg/pwndbg 
+cd pwndbg
+./setup.sh
 sudo apt-get install 
